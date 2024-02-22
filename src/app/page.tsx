0abd52cc67d3f7d8772380caa65ce918e59a309f
mkuranda <kuranda.marek@gmail.com>
2024-02-22 18:52:57 +0100

    Added SCSS preprocessor and its styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import styles from '@/src/styles/test.module.scss';
+
 export default function Home() {
     return (
-        <>Home page</>
+        <>Home page <div className={styles['test-class']}>Xxxxx</div><div className="y-class test-class">Y</div></>
     );
 }
