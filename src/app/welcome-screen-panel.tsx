89ebec559cc71862241389876202df9c6be07423
mkuranda <kuranda.marek@gmail.com>
2024-02-23 11:50:04 +0100

    screen

--- src/app/welcome-screen-panel.tsx
+++ src/app/welcome-screen-panel.tsx
@@ -1,10 +1,15 @@
+import styles from '@/styles/app/welcome-screen.module.scss';
+
 export function WelcomeScreenPanel() {
+    const className = `${styles['panel']} d-flex justify-content-center align-items-center flex-column`;
+
     return (
-        <div className="d-flex">
-            <h1>Yummy</h1>
-            <button></button>
-            <button></button>
-            <button></button>
+        <div className={className}>
+            <h1 className={styles['header-panel']}>Yummy</h1>
+            <div>
+                <button>Szukaj po składnikach</button>
+                <button>Propozycja dnia</button>
+            </div>
         </div>
     );
 }
