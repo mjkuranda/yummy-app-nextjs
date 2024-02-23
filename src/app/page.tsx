f360c13a03f36d8ca5e134b29c250d251fb0837e
mkuranda <kuranda.marek@gmail.com>
2024-02-23 12:32:15 +0100

    footer common component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import { WelcomeScreen } from '@/src/app/welcome-screen';
+import { InformationScreen } from '@/src/app/information-screen';
+import { Footer } from '@/src/components/common/footer';
 
 export default function Home() {
     return (
         <>
             <WelcomeScreen />
+            <InformationScreen title="Meals" description="Easy for users. Containing over X meals!" />
+            <InformationScreen title="Integrating" description="Creates cooking community." />
+            <InformationScreen title="Mealful" description="Integrate various recipesets." />
+            <Footer />
         </>
     );
 }
