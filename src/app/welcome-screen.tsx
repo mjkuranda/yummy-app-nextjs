1f11549d8ab6fc67fbde6be946559223ed6b3bc1
mkuranda <kuranda.marek@gmail.com>
2024-02-23 12:00:55 +0100

    information screen

--- src/app/welcome-screen.tsx
+++ src/app/welcome-screen.tsx
@@ -1,10 +1,14 @@
 import styles from '@/styles/app/welcome-screen.module.scss';
 import { WelcomeScreenPanel } from '@/src/app/welcome-screen-panel';
+import { InformationScreen } from '@/src/app/information-screen';
 
 export function WelcomeScreen() {
     return (
         <main className={styles['welcome-screen']}>
             <WelcomeScreenPanel />
+            <InformationScreen title="Meals" description="Easy for users. Containing over X meals!" />
+            <InformationScreen title="Integrating" description="Creates cooking community." />
+            <InformationScreen title="Mealful" description="Integrate various recipesets." />
         </main>
     );
 }
