c3804f47976498e0af5feff44cc2901888b0f921
Marek Kurańda <89488013+mjkuranda@users.noreply.github.com>
2024-08-22 16:17:46 +0200

    [1.33.0] Meal creation form divided (#34)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.33.0] - 2024-08-22
+### Changed
+- [Marek Kurańda](https://github.com/mjkuranda): Meal form creation layout dividing into two parts.
+
 ## [1.32.0] - 2024-08-22
 ### Added
 - [Marek Kurańda](https://github.com/mjkuranda): `AddButton` component as combination of icon and text.
---
+++ src/app/meals/create/selected-ingredient.tsx
@@ -0,0 +1,27 @@
+import { ListItem, ListItemText } from '@mui/material';
+import { RemoveButton } from '@/src/components/common/remove-button';
+import React from 'react';
+import { Items } from '@/src/components/common/form/input-list';
+
+interface SelectedIngredientProps {
+    index: number;
+    item: [string, string];
+    selectedItems: Items;
+    setSelectedItems: (items: Items) => void;
+}
+
+export function SelectedIngredient({ index, item, selectedItems, setSelectedItems }: SelectedIngredientProps) {
+    const onRemoveIngredient = () => {
+        const modifiedItems = { ...selectedItems };
+        delete modifiedItems[item[0]];
+
+        setSelectedItems(modifiedItems);
+    };
+
+    return (
+        <ListItem key={index}>
+            <ListItemText primary={item[1]} />
+            <RemoveButton label={'Remove'} onClick={onRemoveIngredient} />
+        </ListItem>
+    );
+}
