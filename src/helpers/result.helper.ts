9484939acbe34ef4815b1542cc4010f1068d71f1
mkuranda <kuranda.marek@gmail.com>
2024-08-30 23:53:39 +0200

    meal proposal images

--- src/helpers/result.helper.ts
+++ src/helpers/result.helper.ts
@@ -1,5 +1,10 @@
 import { API_URL } from '@/src/api/methods';
+import { MealProvider } from '@/src/types/api.types';
 
-export function getImageUrlForYummyMeals(imgUrl?: string): string | undefined {
+export function getImageUrlForYummyMeals(provider: MealProvider, imgUrl?: string): string | undefined {
+    if (provider === 'spoonacular') {
+        return imgUrl;
+    }
+
     return imgUrl ? `${API_URL}/images/meals/${imgUrl}` : undefined;
 }
