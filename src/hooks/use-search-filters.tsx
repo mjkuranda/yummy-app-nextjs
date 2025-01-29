5b4ef29e659e5e938887422b3089c6039c22d35c
mkuranda <kuranda.marek@gmail.com>
2025-01-29 15:50:02 +0100

    fixes

--- src/hooks/use-search-filters.tsx
+++ src/hooks/use-search-filters.tsx
@@ -4,6 +4,7 @@ import { SearchFilters } from '@/src/types/search.types';
 import { useSearchParams } from 'next/navigation';
 import { decodeIngredients } from '@/src/helpers/query.helper';
 import { DishType, MealType } from '@/src/types/dish.types';
+import { inferMealTypeBasingOnTime } from '@/src/helpers/search.helper';
 
 export function useSearchFilters(): SearchFilters {
     const searchParams = useSearchParams();
@@ -11,7 +12,7 @@ export function useSearchFilters(): SearchFilters {
     return {
         originalQuery: `ings=${searchParams.get('ings')}&type=${searchParams.get('type')}&dish=${searchParams.get('dish')}`,
         ings: decodeIngredients(searchParams.get('ings')),
-        mealType: (searchParams.get('type') ?? 'any') as MealType,
+        mealType: (searchParams.get('type') ?? inferMealTypeBasingOnTime()) as MealType,
         dishType: (searchParams.get('dish') ?? 'any') as DishType
     };
 }
