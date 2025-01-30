fbf84ec01c8b62efc1c2d2099429cb8f28df5564
Marek Kurańda <89488013+mjkuranda@users.noreply.github.com>
2025-01-30 15:52:55 +0100

    [1.63.0] Language information along with search result (#84)

--- src/app/result/[id]/dish-general.tsx
+++ src/app/result/[id]/dish-general.tsx
@@ -10,6 +10,7 @@ import { DishIngredients } from '@/src/app/result/[id]/dish-ingredients';
 import { DishDescription } from '@/src/app/result/[id]/dish-description';
 import { DishDeletion } from '@/src/app/result/[id]/dish-deletion';
 import { DishEdition } from '@/src/app/result/[id]/dish-edition';
+import { FlagIcon } from '@/src/components/common/flag-icon';
 
 interface DishGeneralProps {
     dish: DetailedDish;
@@ -25,19 +26,7 @@ export function DishGeneral({ dish, description, ingredients, recipe }: DishGene
                 <li>
                     <div className={styles['result-dish-title']}>
                         <h3>{dish.title}</h3>
-                        {dish.language !== 'pl' &&
-                            <img
-                                src="/uk.png"
-                                alt="Flaga UK"
-                                width={48}
-                                title="Danie zostało przetłumaczone. Może zawierać błędy w tłumaczeniu."
-                                data-author={
-                                    <a href="https://www.flaticon.com/free-icons/uk-flag" title="uk flag icons">
-                                        Uk flag icons created by IconsBox - Flaticon
-                                    </a>
-                                }
-                            />
-                        }
+                        {dish.language !== 'pl' && <FlagIcon language={'en'} />}
                     </div>
                     <div className={styles['information-container']}>
                         <span>Czas wykonania:</span>
---
+++ src/components/common/flag-icon.tsx
@@ -0,0 +1,35 @@
+import { Language } from '@/src/types/api.types';
+
+interface FlagIconProps {
+    language: Language;
+    hasTranslationInfo?: boolean;
+}
+
+// @ts-ignore
+const flagInfo: Record<Language, { alt: string, author: JSX.Element, image: string }> = {
+    pl: {
+        alt: 'Flaga Polski',
+        author: <a href="https://www.flaticon.com/free-icons/poland" title="poland icons">Poland icons created by Smashicons - Flaticon</a>,
+        image: 'pl'
+    },
+    en: {
+        alt: 'Flaga UK',
+        author: <a href="https://www.flaticon.com/free-icons/uk-flag" title="uk flag icons">Uk flag icons created by IconsBox - Flaticon</a>,
+        image: 'uk'
+    }
+};
+
+export function FlagIcon({ language, hasTranslationInfo }: FlagIconProps) {
+    const flag = flagInfo[language];
+
+    return (
+        <img
+            style={{ cursor: 'pointer' }}
+            src={`/flags/${flag.image}.png`}
+            alt={flag.alt}
+            width={48}
+            title={hasTranslationInfo ? 'Danie zostało przetłumaczone. Może zawierać błędy w tłumaczeniu.' : 'Język oryginału'}
+            data-author={flag.author}
+        />
+    );
+}
